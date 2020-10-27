298c95c05282fe33dd7791880f23a3aea4d99f94
AnastasiaGaranovich <nastyamadness@gmail.com>
2020-10-27 20:15:43 +0300

    Fixed custom table cell, addition table view controller

--- Habit Helper/HabitTrackerController.swift
+++ Habit Helper/HabitTrackerController.swift
@@ -7,29 +7,44 @@
 
 import UIKit
 
+extension UIColor {
+    
+    static let sand = UIColor(red: 1.0, green: 0.5, blue: 0, alpha: 0.5)
+    
+}
+
 class HabitTrackerController: UITableViewController {
     let items = ["Item 1", "Item2", "Item3", "Item4"]
     let images: [UIImageView] = []
-    let progresses: [UIProgressView] = []
-
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            tableView.register(UINib(nibName: "CustomTableCell", bundle: nil), forCellReuseIdentifier: "CustomTableCell")
-        }
-
-        // MARK: - UITableViewDataSource
-
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return items.count
-        }
-
-        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableCell", for: indexPath) as! CustomTableCell
-
-            cell.habitImage = images[indexPath.row]
-            cell.habitName.text = items[indexPath.row]
-            cell.habitProgress = progresses[indexPath.row]
-
-            return cell
-        }
+    let progresses: [Float] = [0.2, 0.5, 0.6, 1]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UINib(nibName: "CustomTableCell", bundle: nil), forCellReuseIdentifier: "CustomTableCell")
+        tableView.backgroundColor = .sand
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomTableCell", for: indexPath) as! CustomTableCell
+        
+        // cell.habitImage = images[indexPath.row]
+        cell.habitName.text = items[indexPath.row]
+        cell.habitProgress.progress = progresses[indexPath.row]
+        cell.backgroundColor = .sand
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+    
 }
+
