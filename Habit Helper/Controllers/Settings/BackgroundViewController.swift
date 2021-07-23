752ddebb2075f7079d092323c516d20fd01d99b6
AnastasiaGaranovich <nastyamadness@gmail.com>
2021-07-23 11:29:47 +0300

    Create UserInfo view controller and update settings view controller

--- Habit Helper/Controllers/Settings/BackgroundViewController.swift
+++ Habit Helper/Controllers/Settings/BackgroundViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class BackgroundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+		tabBarController?.tabBar.isHidden = true
         setNavigation()
     }
 }
